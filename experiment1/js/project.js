0aee8a1fc18b12463ce1b52c5fecc59f48655f32
jible <jamezman88th@gmail.com>
2024-04-07 22:54:08 -0700

    Done!

--- experiment1/js/project.js
+++ experiment1/js/project.js
@@ -1,34 +1,67 @@
 // project.js - purpose and description here
-// Author: Your Name
-// Date:
+// Author: James Milestone
+// Date: 4/7/2024
 
 // NOTE: This is how we might start a basic JavaaScript OOP project
 
 // Constants - User-servicable parts
 // In a longer project I like to put these in a separate file
 
-// define a class
-class MyProjectClass {
-  // constructor function
-  constructor(param1, param2) {
-    // set properties using 'this' keyword
-    this.property1 = param1;
-    this.property2 = param2;
-  }
-  
-  // define a method
-  myMethod() {
-    // code to run when method is called
-  }
-}
 
 function main() {
-  // create an instance of the class
-  let myInstance = new MyProjectClass("value1", "value2");
-
-  // call a method on the instance
-  myInstance.myMethod();
+  const fillers = {
+  
+    wake: ["wake up super early just to play games in bed.", "wake up at noon!"],
+    breakfast: [" waffles with syrup and whipped cream","eggs and bacon", "Cheereos and orange juice"],
+    activityA: [" get some homework done. Maybe I'll work on my 147 homework", "play some overwatch; I'm so close to masters", "just watch some Youtube. Maybe that will give me ideas"],
+    lunch: ["bowl of mac n cheese", "sandwich"," glass of water"],
+    afternoon: ["draw up some game ideas", "play ping pong with my housemate", "take a nap", " go on a run" ],
+    evening: ["read some comics","clean my room", "do my laundry", "take a nap"],
+    dinner: ["pasta", "pizza", "stew", "salad", "ramen"],
+    activityB: ["some pokemon", "a game of overwatch", "a round of marvel snap", "a run", "some music", "a walk", "some ice cream" ],
+    
+  };
+  
+  const template = `
+  I have so much to do during the week that I only get one day off and it just so happens that that day is tomorrow! How should I spend tomorrow?
+  
+  First, I'll $wake
+  Then I'll start my day with $breakfast.
+  Now that I'm full, I'll be able to $activityA.
+  That took so long that its already lunch. I'll have a $lunch.
+  In the afternoon, I'll $afternoon, then $evening.
+  I hope I can have $dinner for dinner. Then I'll end my day with $activityB before I go to sleep.
+  `;
+  
+  
+  // STUDENTS: You don't need to edit code below this line.
+  
+  const slotPattern = /\$(\w+)/;
+  
+  function replacer(match, name) {
+    let options = fillers[name];
+    if (options) {
+      return options[Math.floor(Math.random() * options.length)];
+    } else {
+      return `<UNKNOWN:${name}>`;
+    }
+  }
+  
+  function generate() {
+    let story = template;
+    while (story.match(slotPattern)) {
+      story = story.replace(slotPattern, replacer);
+    }
+  
+    /* global box */
+    $("#box").text(story);
+  }
+  
+  /* global clicker */
+  $("#clicker").click(generate);
+  
+  generate();
+  
 }
 
-// let's get this party started - uncomment me
-//main();
+main();
